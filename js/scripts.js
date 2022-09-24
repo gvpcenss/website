e3bb1cc6109906e486b782f948fb483d5f494e88
gvpcenss <114252674+gvpcenss@users.noreply.github.com>
2022-09-24 14:30:37 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,30 @@ $(function () {
   $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 });
+
+window.onscroll = () => {
+  let height = document.getElementById("banner").scrollHeight;
+  if(window.scrollY - 70 > height){
+    document.getElementById("banner").style.display = "none";
+    document.getElementById("brand-name").style.color = "#332265"
+  }else{
+    document.getElementById("banner").style.display = "flex";
+    document.getElementById("brand-name").style.color = "#fff";
+  }
+}
+
+let isNavOpen = false;
+
+window.onload = () => isNavOpen = false; 
+window.onresize = () => isNavOpen = false;
+
+const hamburgerBtnHandler = document.getElementById("hamburger-btn");
+
+hamburgerBtnHandler.onclick = () => {
+  isNavOpen = !isNavOpen;
+  if(isNavOpen){
+    document.getElementById("navbarNavAltMarkup").classList.add("hideNavMobile");
+  }else {
+    document.getElementById("navbarNavAltMarkup").classList.remove("hideNavMobile");
+  }
+}
